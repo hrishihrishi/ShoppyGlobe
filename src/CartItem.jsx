50e00c6cc5aa1921d9bd21d0c203729898c6c459
Hrishikesh <hrishi.halesh.18@gmail.com>
2026-01-29 11:58:29 +0530

    + Navbar

---
+++ src/CartItem.jsx
@@ -0,0 +1,5 @@
+export function CartItem() {
+    return (
+        <div>CartItem</div>
+    )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+export function Header() {
+    return (
+        <nav className=" bg-blue-400 flex p-2 px-[4vw] rounded-2xl m-3 justify-between">
+            <div>
+                <img src="src/assets/shopping-cart.png" alt="src/assets/shopping-cart.png" 
+                className="w-[6vw] bg-blue-100 rounded-xl p-1"/>
+            </div>
+            <div className=" flex items-center justify-center">
+                <ul className="flex flex-row text-white text-xl space-x-4 font-bold">
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
